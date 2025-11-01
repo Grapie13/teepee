916d206b253c9e6c98b583a1d0e5a0e025dcface
Gracjan Pietruszyński <20210254+Grapie13@users.noreply.github.com>
2025-11-01 17:22:22 +0100

    fix: make use of free_result in example

--- apps/example.c
+++ apps/example.c
@@ -10,8 +10,10 @@ int main()
 
     printf("Request failed with code %d", error->code);
     printf("Error message: %s", error->message);
+    free_result(result);
     return 1;
   }
 
   printf("Response: %s", result->body);
+  free_result(result);
 }
